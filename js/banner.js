658df4a74c09f330d5688a9484713b81a027e398
XY-ZHOU <731986251@qq.com>
2019-01-14 21:05:27 +0800

    the function of banner

---
+++ js/banner.js
@@ -0,0 +1,59 @@
+$(function (){
+  let timer;
+  let index=0;
+  let imgWidth=$('.banner_lists li').width();
+  setTime();
+  function setTime(){
+    timer=setInterval(function(){
+     countLeft(imgWidth,index);
+     styleOfCircle(index);
+      index++;
+      if(index==4){
+        index=0;
+      }
+    },1000);
+  }
+   $('.banner_circle li').mouseenter(function(){
+      clearInterval(timer);
+       $(this).css('backgroundColor','#ff4902')
+        .siblings('li')
+        .css('backgroundColor','#fff');
+         index=$(this).index();
+         countLeft(imgWidth,index);
+         setTime();
+   });
+
+  $('.arrow-left').click(function(){
+      clearInterval(timer);
+      setTime();
+       index -= 1;
+      if(index<0){
+        index=3;
+      }
+       countLeft(imgWidth,index);
+        styleOfCircle(index);
+    });
+
+    $('.arrow-right').click(function(){
+      clearInterval(timer);
+      setTime();
+      index += 1;
+      if(index==4){
+        index=0;
+      }
+      countLeft(imgWidth,index);
+      styleOfCircle(index);
+    });
+
+});
+
+function countLeft(imgWidth,index){
+  let leftWidth=-imgWidth*index;
+  $('.banner_lists').css('left',leftWidth+'px');
+}
+function styleOfCircle(index){
+   $('.banner_circle li').eq(index)
+        .css('backgroundColor','#ff4902')
+        .siblings('li')
+        .css('backgroundColor','#fff');
+}
