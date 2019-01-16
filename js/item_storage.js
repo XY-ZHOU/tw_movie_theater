b993ac9f0408b0cc49f9d1b8e189b49792c479ef
XY-ZHOU <731986251@qq.com>
2019-01-16 20:04:28 +0800

    function of storaging items

--- js/item_storage.js
+++ js/item_storage.js
@@ -2,4 +2,7 @@ class ItemsStorage {
   constructor(localStorage) {
     this.itemsStorage = localStorage;
   }
+  storageItems(movies) {
+    this.itemsStorage.setItem('movies', JSON.stringify(movies));
+  }
 }
