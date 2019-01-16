9c81bfbfdac07567ef2c06d5bec1df08a08be90c
KC2A3 <947496351@qq.com>
2019-01-17 06:18:43 +0800

    add acquisition method

--- js/details.js
+++ js/details.js
@@ -6,3 +6,12 @@ for (let i = 1; i < movies.length; i++) {
     data = movies[i];
   }
 }
+var template_name = $.templates("#movie_name");
+var htmlOutput_name = template_name.render(data);
+$(".movie_name").html(htmlOutput_name);
+var template_pic = $.templates("#movie_picture");
+var htmlOutput_pic = template_pic.render(data);
+$(".movie_picture").html(htmlOutput_pic);
+var template_info = $.templates("#movie_information");
+var htmlOutput_info = template_info.render(data);
+$(".movie_information").html(htmlOutput_info);
