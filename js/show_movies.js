cdc299588270900b12c2fdbccc3215ab0cb82c77
XY-ZHOU <731986251@qq.com>
2019-01-16 20:27:37 +0800

    the function of page up and down

--- js/show_movies.js
+++ js/show_movies.js
@@ -4,11 +4,27 @@ function showMovies(items) {
   let itemCount = 12;
   let PageCount = Math.ceil(items.length / itemCount);
   let currentPage = 1;
-
   //show the first page
   $('.page p').html("第" + currentPage + "页/共" + PageCount + "页");
   isShowpageBtn(currentPage, PageCount);
   traverseItems(currentPage, itemCount, items);
-
+  previousPageAndNextPage(currentPage,PageCount,itemCount,items);
 }
 
+function previousPageAndNextPage(currentPage,PageCount,itemCount,items){
+  $('.next_page').click(function() {
+    currentPage += 1;
+    console.log(currentPage, PageCount);
+    isShowpageBtn(currentPage, PageCount);
+    $('.movies_message').html('');
+    $('.page p').html("第" + currentPage + "页/共" + PageCount + "页");
+    traverseItems(currentPage, itemCount, items);
+  });
+  $('.previous_page').click(function() {
+    currentPage -= 1;
+    isShowpageBtn(currentPage, PageCount);
+    $('.movies_message').html('');
+    $('.page p').html("第" + currentPage + "页/共" + PageCount + "页");
+    traverseItems(currentPage, itemCount, items);
+  })
+}
