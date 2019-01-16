c435fddfa0c81d815356602bbc8ad617b114c5df
XY-ZHOU <731986251@qq.com>
2019-01-16 20:41:57 +0800

    package function of sort event listener

--- js/get_movies.js
+++ js/get_movies.js
@@ -3,7 +3,10 @@ $(function() {
   itemsStorage.storageItems(movies);
   let allMovies = itemsStorage.getStorageItems();
   showMovies(allMovies);
+  listenerOfSort(itemsStorage);
+});
 
+function listenerOfSort(itemsStorage){
   $(".filter_nav li a").on("click", function(event) {
     $(this).addClass('selected').siblings('a').removeClass('selected');
     $(this).parent('li').siblings('li').find('a').removeClass('selected');
@@ -11,4 +14,4 @@ $(function() {
     let filterMovies = itemsStorage.filterItems(filterWord);
     showMovies(filterMovies);
   });
-});
+}
