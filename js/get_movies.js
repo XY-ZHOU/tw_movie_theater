6cdeaeb7524c657a1f59e9f376b1e3d4c1db2a40
XY-ZHOU <731986251@qq.com>
2019-01-16 20:10:35 +0800

    eventListener of sort

--- js/get_movies.js
+++ js/get_movies.js
@@ -3,4 +3,12 @@ $(function() {
   itemsStorage.storageItems(movies);
   let allMovies = itemsStorage.getStorageItems();
   showMovies(allMovies);
+
+  $(".filter_nav li a").on("click", function(event) {
+    $(this).addClass('selected').siblings('a').removeClass('selected');
+    $(this).parent('li').siblings('li').find('a').removeClass('selected');
+    let filterWord = $(this).text().trim();
+    let filterMovies = itemsStorage.filterItems(filterWord);
+    showMovies(filterMovies);
+  });
 });
