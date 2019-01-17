dc91b2946e8b021dc12620bba3a4ba196b254edb
KC2A3 <947496351@qq.com>
2019-01-17 23:05:21 +0800

    get data using the jsonp method

--- js/createRequest.js
+++ js/createRequest.js
@@ -14,3 +14,26 @@ function viewComments(datas) {
 }</li>`;
   $('.movie_content').append(label)
 }
+
+function getReview(id, callback, count = 2, start = 0) {
+  $.ajax({
+    type: "get",
+    url: `https://api.douban.com/v2/movie/subject/${id}/comments`,
+    dataType: "jsonp",
+    jsonp: "callback",
+    data: {
+      apikey: "0b2bdeda43b5688921839c8ecb20399b",
+      start: start,
+      count: count,
+      client: "",
+      udid: ""
+    },
+    success: function(json) {
+      callback(json);
+    },
+    error: function() {
+      console.log(`failed to get movie comments`);
+    }
+  })
+}
+getReview(1292223, viewComments, 5)
