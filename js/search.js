8ce9b81ef4d90a742fbe7e9c61541d6bad2420cc
DoloresDai <841856753@qq.com>
2019-01-17 14:22:41 +0800

    Modify details

--- js/search.js
+++ js/search.js
@@ -1,67 +1,62 @@
 $(function() {
-      var items = new ItemsStorage(window.localStorage);
-      items.storageItems(MOVIES_DATA);
-      let moviesArr = items.getStorageItems();
-      var typeArr = $(".filter_nav li a");
-      var movies = $("#input").val();
-      $("#btn").click(function() {
-        if (movies) {
-          $("#input").val("");
-          for (let j in typeArr) {
-            if (movies == typeArr[j].id) {
-              //switch to homepape movies pages
-            } else {
-              for (let i in moviesArr) {
-                switch (movies) {
-                  case moviesArr[i].id:
-                  case moviesArr[i].title:
-                  case moviesArr[i].original_title:
-                    //all switch to detail html
-                    // $("")
-                    // location.href = "movie_details.html";
-                    break;
-                  default:
-                    layer.open({
-                      title: '提示信息',
-                      content: '仅支持按类别、电影名称、id查找'
-                    });
-                    window.location.reload();
-                    break;
-                }
-              }
-            };
-          }
+  var items = new ItemsStorage(window.localStorage);
+  items.storageItems(MOVIES_DATA);
+  let moviesArr = items.getStorageItems();
+  var typeArr = $(".filter_nav li a");
+  $("#btn").click(function() {
+    var movies = $("#input").val();
+    if (movies) {
+      $("#input").val("");
+      for (let j in typeArr) {
+        if (movies == typeArr[j].id) {
+          //switch to homepape movies pages
         }
-      });
-      $("#input").keydown(function(e) {
-        if (e.keyCode == 13) {
-          if (movies) {
-            $("#input").val("");
-            for (let j in typeArr) {
-              if (movies == typeArr[j].id) {
-                //switch to homepape movies pages
-              } else {
-                for (let i in moviesArr) {
-                  switch (movies) {
-                    case moviesArr[i].id:
-                    case moviesArr[i].title:
-                    case moviesArr[i].original_title:
-                      //all switch to detail html
-                      // $("")
-                      // location.href = "movie_details.html";
-                      break;
-                    default:
-                      layer.open({
-                        title: '提示信息',
-                        content: '仅支持按类别、电影名称、id查找'
-                      });
-                      window.location.reload();
-                      break;
-                  }
-                }
-              };
-            }
-          }
+      }
+      for (let i in moviesArr) {
+        switch (movies) {
+          case moviesArr[i].id:
+          case moviesArr[i].title:
+          case moviesArr[i].original_title:
+            $(window).attr('location', 'movie_details.html?=' + moviesArr[i].id);
+            break;
+          default:
+            // layer.open({
+            //   title: '提示信息',
+            //   content: '仅支持按类别、电影名称、id查找',
+            //   time: 10000,
+            // });
+            break;
         }
-      });
+      }
     }
+  });
+  $("#input").keydown(function(e) {
+    if (e.keyCode == 13) {
+      var movies = $("#input").val();
+      if (movies) {
+        $("#input").val("");
+        for (let j in typeArr) {
+          if (movies == typeArr[j].id) {
+            //switch to homepape movies pages
+          }
+        }
+        for (let i in moviesArr) {
+          switch (movies) {
+            case moviesArr[i].id:
+            case moviesArr[i].title:
+            case moviesArr[i].original_title:
+              $(window).attr('location', 'movie_details.html?=' + moviesArr[i].id);
+              break;
+            default:
+              // layer.open({
+              //   title: '提示信息',
+              //   content: '仅支持按类别、电影名称、id查找',
+              //   time: 10000,
+              // });
+              break;
+          }
+        }
+      }
+    }
+  });
+});
